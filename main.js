9f02bef902ff87042b32e4ad1e814335d10c7b9b
igorokiscool <igorokiscool@gmail.com>
2022-03-08 14:01:56 +0300

    clean main script

--- main.js
+++ main.js
@@ -4,8 +4,3 @@ import { Application } from "./modules/Application.js";
 const app = new Application();
 
 app.listen();
-// const PORT = process.env.PORT || 3000;
-
-// const server = http.createServer();
-
-// server.listen(PORT, ()=>console.log(`started server on port: ${PORT}`))
